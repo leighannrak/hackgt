576ef842dac8101758a9838bf766ab8ff2e6d953
leighannrak <leighannraksasouk@gmail.com>
2018-10-21 00:04:01 -0400

    azure facial recognition of images

---
+++ hackgt/src/marketing.java
@@ -0,0 +1,85 @@
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.File;
+
+public class marketing{
+    // Replace <Subscription Key> with your valid subscription key.
+    private static final String subscriptionKey = "fcb546bca29844b8a3caf143a43ba814";
+
+    private static final String uriBase =
+            "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+    private static final String imageWithFaces =
+            "{\"url\":\"https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/43951804_1940299606058428_4603545805565984768_n.jpg?_nc_cat=100&_nc_ht=scontent-atl3-1.xx&oh=470097133ce050e041b7499414b442e3&oe=5C54A21B\"}";
+
+    private static final String faceAttributes =
+            "age,gender,emotion";
+
+    public static void main(String[] args)
+    {
+        HttpClient httpclient = new DefaultHttpClient();
+
+        try
+        {
+            URIBuilder builder = new URIBuilder(uriBase);
+
+            // Request parameters. All of them are optional.
+            builder.setParameter("returnFaceId", "true");
+            builder.setParameter("returnFaceLandmarks", "false");
+            builder.setParameter("returnFaceAttributes", faceAttributes);
+
+            // Prepare the URI for the REST API call.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity reqEntity = new StringEntity(imageWithFaces);
+            request.setEntity(reqEntity);
+
+            // Execute the REST API call and get the response entity.
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                // Format and display the JSON response.
+                System.out.println("REST Response:\n");
+
+                String jsonString = EntityUtils.toString(entity).trim();
+                if (jsonString.charAt(0) == '[') {
+                    JSONArray jsonArray = new JSONArray(jsonString);
+                    System.out.println(jsonArray.toString(2));
+                }
+                else if (jsonString.charAt(0) == '{') {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    JSONObject docs = jsonObject.getJSONArray("infile");
+
+                    File file = new
+
+
+                    System.out.println(jsonObject.toString(2));
+                } else {
+                    System.out.println(jsonString);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
